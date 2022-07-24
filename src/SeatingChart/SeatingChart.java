0f3f454a2e0b6e142954cda954a27f7cd1bb3df0
alessandro-pianetta <alessandro.pianetta@gmail.com>
2022-07-24 13:43:11 -0700

    Writes first attempt at switchStudent method

--- src/SeatingChart/SeatingChart.java
+++ src/SeatingChart/SeatingChart.java
@@ -1,4 +1,28 @@
 package SeatingChart;
 
 public class SeatingChart {
+    Student[][] seatingChart = new Student[4][5];
+
+    public void addStudent(String first, String last, int row, int seat) {
+        seatingChart[row][seat] = new Student(first, last);
+    }
+
+    public void switchStudents(String first, String last, int targetRow, int targetSeat) {
+        // TODO: Check to make sure that this ref v doesn't change
+        Student studentToSwitch = seatingChart[targetRow][targetSeat];
+        Student newStudent;
+
+        for (Student[] row:this.seatingChart) {
+            for (Student seat:row) {
+                if (seat.firstName.equals(first) && seat.lastName.equals(last)) {
+                    newStudent = new Student(first, last);
+                    seatingChart[targetRow][targetSeat] = newStudent;
+                    seat = studentToSwitch;
+                    return;
+                }
+            }
+        }
+    }
 }
+
+
